9b4503a1c1c3b547971254c773f41bb132c280e2
SnowyField1906 <aswdqe1x@gmail.com>
2024-03-10 16:27:35 +0700

    fix env

--- app.module.ts
+++ app.module.ts
@@ -9,37 +9,37 @@ import configuration from '@config'
 import * as controllers from '@controllers'
 import * as services from '@services'
 import {
-	Commitment,
-	CommitmentSchema,
-	Secret,
-	SecretSchema,
-	Wallet,
-	WalletSchema,
+    Commitment,
+    CommitmentSchema,
+    Secret,
+    SecretSchema,
+    Wallet,
+    WalletSchema,
 } from '@schemas'
 
 @Module({
-	imports: [
-		HttpModule,
-		ConfigModule.forRoot({
-			load: [configuration],
-		}),
-		MongooseModule.forRootAsync({
-			imports: [ConfigModule],
-			useFactory: async (configService: ConfigService) => {
-				return {
-					uri: configService.get<string>('database.mongoUri'),
-				}
-			},
-			inject: [ConfigService],
-		}),
-		MongooseModule.forFeature([
-			{ name: Commitment.name, schema: CommitmentSchema },
-			{ name: Wallet.name, schema: WalletSchema },
-			{ name: Secret.name, schema: SecretSchema },
-		]),
-		ConfigModule,
-	],
-	controllers: [].concat(Object.values(controllers)),
-	providers: [].concat(Object, Object.values(services), GoogleVerifier),
+    imports: [
+        HttpModule,
+        ConfigModule.forRoot({
+            load: [configuration],
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    uri: configService.get<string>('database.mongoUri'),
+                }
+            },
+            inject: [ConfigService],
+        }),
+        MongooseModule.forFeature([
+            { name: Commitment.name, schema: CommitmentSchema },
+            { name: Wallet.name, schema: WalletSchema },
+            { name: Secret.name, schema: SecretSchema },
+        ]),
+        ConfigModule,
+    ],
+    controllers: [].concat(Object.values(controllers)),
+    providers: [].concat(Object, Object.values(services), GoogleVerifier),
 })
-export class AppModule { }
+export class AppModule {}
--- config/index.ts
+++ config/index.ts
@@ -1,35 +1,20 @@
 import * as dotenv from 'dotenv'
-dotenv.config()
 
 const isProduction = process.env.NODE_ENV === 'production'
+dotenv.config({ path: isProduction ? '.env' : '.env.local' })
 
-const nodesProduction = [
+const nodes = [
     {
         id: 1,
-        url: process.env.NODE1_URL_PRODUCTION,
+        url: process.env.NODE1_URL,
     },
     {
         id: 2,
-        url: process.env.NODE2_URL_PRODUCTION,
+        url: process.env.NODE2_URL,
     },
     {
         id: 3,
-        url: process.env.NODE3_URL_PRODUCTION,
-    },
-]
-
-const nodesLocal = [
-    {
-        id: 1,
-        url: process.env.NODE1_URL_LOCAL,
-    },
-    {
-        id: 2,
-        url: process.env.NODE2_URL_LOCAL,
-    },
-    {
-        id: 3,
-        url: process.env.NODE3_URL_LOCAL,
+        url: process.env.NODE3_URL,
     },
 ]
 
@@ -49,18 +34,17 @@ const nodePrivateKeys = [
 ]
 
 const productionConfig = {
-    url: nodesProduction.find((node) => node.id === Number(process.env.NODE_ID))
-        .url,
+    url: nodes.find((node) => node.id === Number(process.env.NODE_ID)).url,
     port: 3000,
     database: {
         mongoUri: process.env.MONGO_URI,
     },
     privateKey: process.env.PRIVATE_KEY,
-    nodes: nodesProduction,
+    nodes: nodes,
 }
 
 const localConfig = {
-    url: nodesLocal.find((node) => node.id === Number(process.env.NODE_ID)).url,
+    url: nodes.find((node) => node.id === Number(process.env.NODE_ID)).url,
     port: 3000 + Number(process.env.NODE_ID),
     database: {
         mongoUri: `mongodb+srv://kiet1618:12052002@kltn.mbww3bu.mongodb.net/node${process.env.NODE_ID}?retryWrites=true&w=majority`,
@@ -68,7 +52,7 @@ const localConfig = {
     privateKey: nodePrivateKeys.find(
         (node) => node.id === Number(process.env.NODE_ID)
     ),
-    nodes: nodesLocal,
+    nodes: nodes,
 }
 
 export default () => (isProduction ? productionConfig : localConfig)
